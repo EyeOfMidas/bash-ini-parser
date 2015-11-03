4f593e85e44d6e4e7beee9fd6c7b6c7064d97289
Justin Gilman <jgilman@qinteractive.com>
2015-11-03 11:35:30 -0500

    added in dynamic file names to prevent multiple ini file conflicts

--- example.sh
+++ example.sh
@@ -1,6 +1,19 @@
 . bash-ini-parser
-readini config_example.ini
+readini "config_example.ini" "main"
+readini "config2_example.ini" "pool"
 
+echo "===Contents of main config==="
+switch_file "main"
+for section in `get_sections`; do
+	keys=`get_keys "$section"`
+	echo [$section]
+	for key in $keys; do
+		echo $key = `get_value "$section" "$key"`
+	done
+done
+echo ""
+echo "===Contents of pool config==="
+switch_file "pool"
 for section in `get_sections`; do
 	keys=`get_keys "$section"`
 	echo [$section]
@@ -9,3 +22,16 @@ for section in `get_sections`; do
 	done
 done
 
+echo ""
+echo "==Loaded from file main=="
+switch_file "main"
+load_section "general"
+echo var1 = $var1
+echo var2 = $var2
+
+echo ""
+echo "==Loaded from file pool=="
+switch_file "pool"
+load_section "general"
+echo var1 = $var1
+echo var2 = $var2
